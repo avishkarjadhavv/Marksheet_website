b365573540dbd9315273261147e3037a821b0b3e
avishkarjadhavv <omkaravishkar@gmail.com>
2026-09-02 15:21:34 +0530

    Connect frontend to deployed API

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/api/marks";
+const API_URL = "https://marksheet-website-tdkw.vercel.app/api/marks";
 
 const prnInput = document.getElementById("prn");
 const subjectSelect = document.getElementById("subject");
